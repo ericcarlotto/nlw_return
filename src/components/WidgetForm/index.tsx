5822028daa52e5551d167bbea605b4c9aa3de474
Erc PC <ericitapiruba@hotmail.com.br>
2022-05-04 13:35:05 -0300

    Uploading the last part of the nlw coding camp day two where we have finished the widget frontend

--- src/components/WidgetForm/index.tsx
+++ src/components/WidgetForm/index.tsx
@@ -7,6 +7,7 @@ import ideaImageUrl from "../../assets/idea.svg";
 import thoughtImageUrl from "../../assets/thought.svg";
 import { FeedbackTypeStep } from "./Steps/FeedbackTypeStep";
 import { FeedbackContentStep } from "./Steps/FeedbackContentStep";
+import { FeedbackSuccesStep } from "./Steps/FeedbackSuccesStep";
 
 
 // Icones de feedback
@@ -38,20 +39,29 @@ export type FeedbackType = keyof typeof feedbackTypes;
 
 export function WidgetForm() {
   const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null)
+  const [feedbackSent, setFeedbackSent] = useState(false)
 
   function handleRestartFeedback() {
+    setFeedbackSent(false);
     setFeedbackType(null);
   }
 
   return (
     <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
-      { !feedbackType ? (
-        <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType} />
-       ): (
-        <FeedbackContentStep 
-          feedbackType={feedbackType} 
-          onFeedbackRestartRequested={handleRestartFeedback}
-        />
+      { feedbackSent ? (
+        <FeedbackSuccesStep onFeedbackRestartRequested={handleRestartFeedback} />
+      ) : (
+        <>
+          { !feedbackType ? (
+            <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType} />
+          ): (
+            <FeedbackContentStep 
+              feedbackType={feedbackType} 
+              onFeedbackRestartRequested={handleRestartFeedback}
+              onFeedbackSent={() => setFeedbackSent(true)}
+            />
+          )}
+        </>
       )}
 
       <footer>
